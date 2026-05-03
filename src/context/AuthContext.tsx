8000282e562fec8fbe7de43b438c5d4eb160a0ac
Firee <newowner@News-MacBook-Pro.local>
2026-05-03 18:56:03 -0400

    add AuthProvider to manage global authentication state

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,3 +1,33 @@
 // GLOBAL AUTHENTICATION STATE
 
 
+import React, { createContext, useState, useContext, children } from "react";
+
+
+
+interface AuthContextType {
+    isAuthenticated: boolean;
+    login: () => void;
+    logout: () => void;
+
+}
+
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    // single source of truth for login status 
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+
+    //login and logout toggle boolean when state schanges 
+    const login = () => setIsAuthenticated(true);
+    const logout = () => setIsAuthenticated(false);
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
